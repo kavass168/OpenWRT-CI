9a747fa9e34c5863932af6556f1875fab678860c
kavass168 <631638801@qq.com>
2025-12-31 13:01:40 +0800

    Update filogic.mk

--- hy3000/filogic.mk
+++ hy3000/filogic.mk
@@ -172,14 +172,14 @@ define Device/philips_hy3000
   DEVICE_DTS_LOADADDR := 0x43f00000
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 \
 	automount f2fsck mkf2fs
-  SUPPORTED_DEVICES += philips,hy3000
+  # SUPPORTED_DEVICES += philips,hy3000
   KERNEL_LOADADDR := 0x44000000
   KERNEL := kernel-bin | gzip
   KERNEL_INITRAMFS := kernel-bin | lzma | \
         fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
   KERNEL_INITRAMFS_SUFFIX := -recovery.itb
-  KERNEL_IN_UBI := 1
-  UBOOTENV_IN_UBI := 1
+  # KERNEL_IN_UBI := 1
+  # UBOOTENV_IN_UBI := 1
   IMAGES := sysupgrade.itb
   IMAGE_SIZE := $$(shell expr 64 + $$(CONFIG_TARGET_ROOTFS_PARTSIZE))m
   IMAGE/sysupgrade.itb := append-kernel | \
